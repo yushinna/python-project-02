57c4f78ca01be547d5cb416fee9e48540fc232b4
yushinna <42777142+yushinna@users.noreply.github.com>
2020-08-07 18:34:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -28,26 +28,27 @@ def split_players_by_experience(players):
     players_inexperienced = []
     players_experienced = []
     for player in players:
-        if player['experience'] == False:
+        if player['experience'] is False:
             players_inexperienced.append(player)
-        if player['experience'] == True:
+        if player['experience'] is True:
             players_experienced.append(player)
     return players_inexperienced, players_experienced
 
 
 def create_dict_team_and_players(players, teams):
-    num_players_per_team = int(len(players) /
-                               len(teams))
+    num_players_per_team = int(len(players) / len(teams))
     dict_team_and_players = {}
     start = 0
     for team in teams:
-        dict_team_and_players[team] = players[start:start+num_players_per_team]
+        dict_team_and_players[team] = players[
+            start: start + num_players_per_team
+        ]
         start += num_players_per_team
     return dict_team_and_players
 
 
 def index_to_team(index, teams):
-    return teams[index-1]
+    return teams[index - 1]
 
 
 def count_num_players(list_selected_team_and_players):
@@ -64,7 +65,7 @@ def extract_player_names(list_selected_team_and_players):
 def count_num_inexperienced(list_selected_team_and_players):
     count = 0
     for player in list_selected_team_and_players:
-        if player['experience'] == False:
+        if player['experience'] is False:
             count += 1
     return count
 
@@ -100,14 +101,18 @@ def show_team_stat():
                 selected_team = index_to_team(int(index), constants.TEAMS)
                 # merge inexperienced players and experienced players
                 list_selected_team_and_players = dict_team_and_players_inexperienced[selected_team]\
-                                                 + dict_team_and_players_experienced[selected_team]
+                    + dict_team_and_players_experienced[selected_team]
                 # create stat
                 num_players = count_num_players(list_selected_team_and_players)
-                list_player_names = extract_player_names(list_selected_team_and_players)
-                num_inexperienced = count_num_inexperienced(list_selected_team_and_players)
+                list_player_names = extract_player_names(
+                    list_selected_team_and_players)
+                num_inexperienced = count_num_inexperienced(
+                    list_selected_team_and_players)
                 num_experienced = num_players - num_inexperienced
-                average_height = calculate_total_height(list_selected_team_and_players) / num_players
-                list_gurdian_names = extract_guardian_names(list_selected_team_and_players)
+                average_height = calculate_total_height(
+                    list_selected_team_and_players) / num_players
+                list_gurdian_names = extract_guardian_names(
+                    list_selected_team_and_players)
                 # print stat
                 clear_screen()
                 print('Team: {} Stats'.format(selected_team))
@@ -115,9 +120,11 @@ def show_team_stat():
                 print('Total players: {}\n'.format(num_players))
                 print('Players on Team: ')
                 print(' {}\n'.format(', '.join(map(str, list_player_names))))
-                print('Number of inexperienced players: {}\n'.format(num_inexperienced))
+                print('Number of inexperienced players: {}\n'.format(
+                    num_inexperienced))
                 print('Number of experienced players: {}\n'.format(num_experienced))
-                print('Average height: {0:.2f} inches\n'.format(average_height))
+                print('Average height: {0:.2f} inches\n'.format(
+                    average_height))
                 print('Guardians of all the players on that team: ')
                 print(' {}\n'.format(', '.join(map(str, list_gurdian_names))))
                 input('Press ENTER to continue... ')
@@ -130,9 +137,12 @@ def show_team_stat():
 if __name__ == "__main__":
     # load and clean the data
     updated_players = clean_data(constants.PLAYERS)
-    players_inexperienced, players_experienced = split_players_by_experience(updated_players)
-    dict_team_and_players_inexperienced = create_dict_team_and_players(players_inexperienced, constants.TEAMS)
-    dict_team_and_players_experienced = create_dict_team_and_players(players_experienced, constants.TEAMS)
+    players_inexperienced, players_experienced = split_players_by_experience(
+        updated_players)
+    dict_team_and_players_inexperienced = create_dict_team_and_players(
+        players_inexperienced, constants.TEAMS)
+    dict_team_and_players_experienced = create_dict_team_and_players(
+        players_experienced, constants.TEAMS)
     while True:
         clear_screen()
         print('\nBASKETBALL TEAM STATS TOOL\n')
